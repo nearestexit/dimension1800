6e4a675ccc282e1b0961c4dd77dbec2ac714366d
nearestexit <nearestexit1@gmail.com>
2024-12-04 20:18:04 -0800

    Add Vial firmware

---
+++ firmware/dimension1800/config.h
@@ -0,0 +1,14 @@
+// Copyright 2024 nearestexit (@nearestexit)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/* key matrix size */
+#define MATRIX_ROWS 11
+#define MATRIX_COLS 12
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 5
+
+#define LED_CAPS_LOCK_PIN GP13
+#define LED_PIN_ON_STATE 0
---
+++ firmware/dimension1800/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+
+#define VIAL_KEYBOARD_UID {0xE5, 0x3F, 0x62, 0x5F, 0x94, 0x53, 0xD6, 0x1A}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 0 }
